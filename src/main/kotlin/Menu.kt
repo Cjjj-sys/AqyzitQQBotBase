d272208e220f0c745372e65d9c3bdf410cbe8c40
KangKang <ws2005ncsd@outlook.com>
2022-07-18 21:43:19 +0800

    [+]菜单输入的命令加/

--- src/main/kotlin/Menu.kt
+++ src/main/kotlin/Menu.kt
@@ -5,14 +5,14 @@ import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.message.data.buildMessageChain
 
 class Menu constructor(vararg menuItems: MenuItem) {
-    var menuItems: MutableList<MenuItem> = menuItems.asList().toMutableList()
+    private var menuItems: MutableList<MenuItem> = menuItems.asList().toMutableList()
 
     fun toMessageChain() : MessageChain{
         var chain = buildMessageChain {
             +PlainText("===== 菜单 =====\n")
         }
         for (i in 0 until menuItems.count()) {
-            chain += PlainText("${i+1}: ${menuItems[i].name} ${menuItems[i].command.primaryName}\n")
+            chain += PlainText("${i+1}: /${menuItems[i].name} ${menuItems[i].command.primaryName}\n")
         }
         chain += PlainText("===== 菜单 =====")
         return chain
